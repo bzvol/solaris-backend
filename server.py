0de817e4e0790e2a5f3f22181f8681cc89740db1
bzvol <zvolenszki.benjamin@gmail.com>
2023-03-09 07:20:20 +0100

    regex magic

--- server.py
+++ server.py
@@ -1,53 +1,66 @@
 from flask import Flask, request, Response
 import json
-import hu_core_news_md as hu
 from emmorphpy import EmMorphPy
-
+import re
 
 app = Flask(__name__)
-nlp = hu.load()
-# em = EmMorphPy()
+em = EmMorphPy()
+grouping_pattern = re.compile(r"(?P<prev>[a-záéíőűöüóú]+\+)?(?P<word>[a-záéíőűöüóú]+)\[(?P<wordtype>[^]]+)]=")
 
-@app.route('/')
-def morphology():
-    if request.method != 'POST':
-        # return {'error': 'Only POST requests are allowed.'}
-        return Response({'error':'Only POST requests are allowed.'}, status=405)
-    
-    args = request.args
-    word = args.get('word')
-    sentence = args.get('sentence')
-    if len(args == 0) or (word and sentence):
-        # return {'error': 'Please provide a word or a sentence.'}
-        return Response({'error':'Please provide a word or a sentence.'}, status=400)
-    
-    if word:
-        analyses = em.stem(word)
-        l = []
-        for analysis in analyses:
-            l.append({
-                'stem': analysis[0],
-                'affixes': analysis[1][1:-1].split('][')
+
+@app.route('/api/', methods=['GET', 'POST'])
+def morph_analysis():
+    text = request.args.get('text')
+    if len(request.args) == 0 or text is None:
+        res = {"error": "Please provide a text or word."}
+        return Response(res, status=400, mimetype='application/json')
+
+    words = []
+    for word in text.split():
+        words.append(analyze_word(word))
+
+    res = {'text': text, 'words': words}
+    return Response(json.dumps(res), status=200, mimetype='application/json')
+
+
+def analyze_word(word):
+    data = dict(word=word, analyses=[])
+
+    pos = ['/N', '/V', '/Adj', '/Adv']
+    analyses = em.analyze(word)
+    for analysis in analyses:
+        analysis_data = dict(prefixes=[], compound_parts=[])
+        matches = grouping_pattern.findall(analysis)
+
+        i = 0
+        while matches[i][2] not in pos:
+            prefix, prefix_type = matches[i][1], matches[i][2]
+            analysis_data['prefixes'].append({
+                'part': prefix,
+                'type': prefix_type
             })
-        return json.dumps(l)
-            
-    if sentence:
-        doc = nlp(sentence)
-        l = []
-        for token in doc:
-            l.append({
-                'text': token.text,
-                'lemma': token.lemma_,
-                'morphology': token.morph,
-                'pos': token.pos_,
-                'tag': token.tag_,
-                'dep': token.dep_,
-            })
-        return json.dumps(l)
-    
-    # return 400 and some error
-    # return {'error': 'Something went wrong.'}
-    return Response({'error':'Something went wrong.'}, status=400)
+            i += 1
+
+        current_part = None
+        for _, word_part, word_type in matches[i:]:
+            if word_type in pos:
+                if current_part is not None:
+                    analysis_data['compound_parts'].append(current_part)
+
+                current_part = {
+                    'root': {'part': word_part, 'type': word_type},
+                    'suffixes': []
+                }
+                continue
+
+            current_part['suffixes'].append({
+                'part': word_part, 'type': word_type})
+
+        if current_part is not None:
+            analysis_data['compound_parts'].append(current_part)
+        data['analyses'].append(analysis_data)
+
+    return data
 
 
 if __name__ == '__main__':
